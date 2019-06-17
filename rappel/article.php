b4f29fd8f68d1dd7d3a5dee14bb47740532f72a7
Moustakime KIFIA <mkifia@icloud.com>
2019-06-17 17:45:10 +0200

    affichage article et modification

--- rappel/article.php
+++ rappel/article.php
@@ -5,19 +5,44 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Article</title>
 </head>
 <body>
 
-    <form action="server.php" method="post">
+    <?php
+    require 'connect.php';
 
-        Titre : <input type="text" name="title"></br>
-        Auteur : <input type="text" name="author"></br>
-        Message : <input type="text" name="message"></br>
+    function getArticle($dbh, $id) {
+        $stm = $dbh->query('SELECT * FROM article WHERE id=' . $_GET['id']);
+        return $stm->fetch();
+    }
 
-        <input type="hidden" name="verb" value="add">
-        <input type="submit" value="Envoyer" />
-    </form>
+    $article = getArticle($dbh, $_GET['id']);
+    $id = $article['id'];
+    $titre = $article['titre'];
+    $auteur = $article['auteur'];
+    $message = $article['message'];
+
+    if (isset($_GET['verb']) && $_GET['verb'] === 'get') {
+        echo "
+           article $id rédigé par $auteur pour titre $titre et pour message $message
+        ";
+    } elseif (isset($_GET['verb']) && $_GET['verb'] === 'put') {
+        echo "
+            <form action=\"server.php\" method=\"post\">
+
+                Titre : <input type=\"text\" name=\"title\" value=\"$titre\"></br>
+                Auteur : <input type=\"text\" name=\"author\" value=\"$auteur\"></br>
+                Message : <input type=\"text\" name=\"message\" value=\"$message\"></br>
+                <input type=\"hidden\" name=\"id\" value=\"$id\"></br>
+        
+                <input type=\"hidden\" name=\"verb\" value=\"put\">
+                <input type=\"submit\" value=\"Envoyer\" />
+            </form>
+        ";
+    }
+
+    ?>
 
 </body>
 </html>
--- rappel/server.php
+++ rappel/server.php
@@ -18,6 +18,23 @@ function addArticle($dbh, $article) {
 
 }
 
+function updateArticle($dbh, $article) {
+
+    $titre = isset($article['titre']) ? $article['titre'] : 'titre vide';
+    $auteur = isset($article['auteur']) ? $article['auteur'] : 'auteur inconnu';
+    $message = isset($article['message']) ? $article['message'] : 'message vide';
+    $id = isset($article['id']) ? $article['id'] : 0;
+
+    $stm = $dbh->prepare('UPDATE article SET titre=?, auteur=?, message=? WHERE id=?');
+    $stm->execute(array(
+        $titre,
+        $auteur,
+        $message,
+        $id
+    ));
+
+}
+
 if (isset($_POST['verb']) && $_POST['verb'] === 'add') {
     $article = [];
     $article['titre'] = $_POST['title'];
@@ -26,3 +43,14 @@ if (isset($_POST['verb']) && $_POST['verb'] === 'add') {
 
     addArticle($dbh, $article);
 }
+
+if (isset($_POST['verb']) && $_POST['verb'] === 'put') {
+    $article = [];
+    $article['titre'] = $_POST['title'];
+    $article['auteur'] = $_POST['author'];
+    $article['message'] = $_POST['message'];
+    $article['id'] = (int) $_POST['id'];
+
+    updateArticle($dbh, $article);
+}
+
