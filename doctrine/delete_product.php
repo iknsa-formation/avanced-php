a72efcfe205453d0222e805f1b65b6007acd8380
Moustakime KIFIA <mkifia@icloud.com>
2019-06-19 15:44:46 +0200

    update and delete product

--- doctrine/delete_product.php
+++ doctrine/delete_product.php
@@ -5,12 +5,13 @@ require_once "src/Entity/Product.php";
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = $argv[1];
-$newName = $argv[2];
-
 $productRepository = $entityManager->getRepository('Entity\Product');
 $product = $productRepository->find($id);
+
 if ($product === NULL) {
-    echo "Product $id n'existe pas";
+    echo "Product $id n'existe pas \n";
+    exit();
 }
-$product->setName($newName);
+
+$entityManager->remove($product);
 $entityManager->flush();
