c14c200c85e3cb8503203699852476a2d1540f41
Moustakime KIFIA <mkifia@icloud.com>
2019-06-20 13:42:37 +0200

    remove the useless require

--- doctrine/bootstrap.php
+++ doctrine/bootstrap.php
@@ -3,8 +3,6 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
-require_once "vendor/autoload.php";
-
 $lisDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration(
     array(__DIR__."/src"),
--- doctrine/create_product.php
+++ doctrine/create_product.php
@@ -2,7 +2,6 @@
 // create_product.php
 
 require_once __DIR__ . "/vendor/autoload.php";
-require_once "src/Entity/Product.php";
 require_once "bootstrap.php";
 
 use Entity\Product;
